f52b156991bda4ae58a83c71482ca469b43b3c0f
Hexagon-77 <serbanp733@gmail.com>
2022-06-30 14:45:33 +0300

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,10 +17,28 @@ constructor(props) {
       district: 0,
     },
     pageVal: 1,
+    searchFilter: "",
+    lastPage: false,
   };
 }
 
-refreshList() {
+generateUrl = (url, params) => {
+  var i = 0, key;
+  for (key in params) {
+      if (i === 0) {
+          url += "?";
+      } else {
+          url += "&";
+      }
+      url += key;
+      url += '=';
+      url += params[key];
+      i++;
+  }
+  return url;
+}
+
+refreshList = () => {
     axios
       .get("/api/customers/")
       .then((res) => this.setState({ customerList: res.data }))
@@ -31,7 +49,7 @@ componentDidMount() {
   this.refreshList();
 }
 
-handleSubmit(item) {
+handleSubmit = (item) => {
     this.toggle();
 
     if (item.id) {
@@ -40,15 +58,32 @@ handleSubmit(item) {
         .then((res) => this.refreshList());
       return;
     }
+
     axios
       .post("/api/customers/", item)
       .then((res) => this.refreshList());
 };
 
-handleDelete(item) {
+handleDelete = (item) => {
+    alert("delete " + item.id);
+    
     axios
-      .delete(`/api/customers/${item.id}/`)
+      .delete("/api/customers/", item)
       .then((res) => this.refreshList());
+};
+
+changePage = (i) => {
+  if (this.state.lastPage && i > 0) return;
+  if (this.state.pageVal + i - 1 < 0) return;
+  this.state.pageVal += i;
+
+  if (i == 0) {
+    this.state.pageVal = 1;
+    this.state.searchFilter = "";
+  }
+
+  if (this.state.searchFilter) window.location = this.generateUrl("http://localhost:3000", {p: this.state.pageVal-1, s: this.state.searchFilter})
+  else window.location = this.generateUrl("http://localhost:3000", {p: this.state.pageVal-1})
 };
 
 renderItems = () => {
@@ -58,6 +93,7 @@ renderItems = () => {
     if (page) this.state.pageVal = parseInt(page) + 1;
 
     var newItems = this.state.customerList;
+    this.state.searchFilter = query;
 
     if (query != null) {
       query = query.toLowerCase();
@@ -73,8 +109,12 @@ renderItems = () => {
     }
 
     newItems.sort((a, b) => b.id - a.id)
-    newItems = newItems.slice(5 * page, 5 * page + 5);
 
+    const pagination = 25;
+    var sliced = newItems.slice(pagination * page, pagination * page + pagination);
+    if (sliced.includes(newItems[newItems.length - 1])) this.state.lastPage = true;
+    newItems = sliced;
+    
     return newItems.map((item) => (
       <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
         <div>
@@ -83,7 +123,7 @@ renderItems = () => {
         </div>
         <div>
           <button className="btn btn-secondary">Edit</button>&emsp;
-          <button className="btn btn-danger" onClick={this.handleDelete(item)}>Delete</button>
+          <button className="btn btn-danger" onClick={() => this.handleDelete(item)}>Delete</button>
         </div>
       </li>
     ));
@@ -102,7 +142,10 @@ render() {
       </ul>
 
       <br></br><br></br>
-      <button className="btn btn-primary">⬅</button>&emsp;<button className="btn btn-primary">➡</button>&emsp;Page {this.state.pageVal}
+        <button className="btn btn-primary" onClick={() => this.changePage(-1)}>⬅</button>&emsp;
+        <button className="btn btn-primary" onClick={() => this.changePage(1)}>➡</button>&emsp;
+        Page <b>{this.state.pageVal}</b>&emsp;
+        <button className="btn btn-primary" onClick={() => this.changePage(0)}>⌂</button>
       <br></br><br></br>
     </main>
   ); }
